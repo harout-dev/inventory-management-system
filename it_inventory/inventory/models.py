18a1049cfc77592425e226258bbf900d6a52a47d
harout-dev <75249437+harout-dev@users.noreply.github.com>
2021-03-07 00:47:07 +0200

    add the models

--- it_inventory/inventory/models.py
+++ it_inventory/inventory/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+
+    device_choices = (
+        ('laptop', 'LAPTOP'), ('mobile', 'MOBILE'), ('tab', 'TAB')
+    )
+
+    manufacturer_choices = (
+        ('lenovo', 'LENOVO'), ('samsung', 'SAMSUNG'), ('acer', 'ACER'), ('macbook','MACBOOK'),
+        ('huawei','HUAWEI'), ('hp', 'HP'), ('asus', 'ASUS'),('nokia', 'NOKIA')
+    )
+    device = models.CharField(max_length=10, choices = device_choices)
+    manufacturer = models.CharField(max_length=100 , blank= False, choices = manufacturer_choices)
+    serial_number = models.CharField(max_length=100, blank= False)
+    purchased_date = models.DateField()
+    employee_name = models.CharField(max_length=100)
+    employee_mobile = models.IntegerField()
+    handover_date = models.DateField()
+
+
+
